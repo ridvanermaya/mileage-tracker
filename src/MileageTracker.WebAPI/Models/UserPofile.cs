75fa41415043895c6cb48acc7dd08080bf0f9b71
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-21 10:11:06 -0500

    Created UserProfile model

---
+++ src/MileageTracker.WebAPI/Models/UserPofile.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace MileageTracker.WebAPI.Models
+{
+    [Table("UserProfile")]
+    public class UserPofile
+    {
+        [Key]
+        public int UserProfileId { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [ForeignKey(nameof(Address))]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
