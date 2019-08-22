d69530ba5415a7768bd1a0c47d3554830d70b0d5
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-22 13:20:45 -0500

    Created AddressController

---
+++ src/MileageTracker.WebAPI/Controllers/AddressController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MileageTracker.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressController : ControllerBase
+    {
+        
+    }
+}
