5da9911b0ca58839338f3ab7b3c4a00d30829475
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-22 13:18:14 -0500

    Added PostMileageRecord and DeleteMileageRecord Methods

--- src/MileageTracker.WebAPI/Controllers/MileageRecordController.cs
+++ src/MileageTracker.WebAPI/Controllers/MileageRecordController.cs
@@ -27,5 +27,52 @@ namespace MileageTracker.WebAPI.Controllers
 
             return mileageRecords;
         }
+
+        [HttpGet("{MileageRecordId}")]
+        public async Task<ActionResult<MileageRecord>> GetMileageRecord(int mileageRecordId)
+        {
+            var mileageRecord = await _context.MileageRecords.FirstOrDefaultAsync(x => x.MileageRecordId == mileageRecordId);
+
+            if (mileageRecord == null)
+            {
+                return NotFound();
+            }
+            return mileageRecord;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MileageRecord>> PostMileageRecord(MileageRecord mileageRecord)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            MileageRecord newMileageRecord = new MileageRecord();
+            
+            newMileageRecord.Service = mileageRecord.Service;
+            newMileageRecord.Mileage = mileageRecord.Mileage;
+            newMileageRecord.StartDateTime = mileageRecord.StartDateTime;
+            newMileageRecord.EndDateTime = mileageRecord.EndDateTime;
+            newMileageRecord.UserId = userId;
+
+            _context.MileageRecords.Add(newMileageRecord);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{MileageRecordId}")]
+        public async Task<ActionResult> DeleteMileageRecord(int mileageRecordId)
+        {
+            var foundMileageRecord = await _context.MileageRecords.FirstOrDefaultAsync(x => x.MileageRecordId == mileageRecordId);
+
+            if (foundMileageRecord == null)
+            {
+                return NotFound();
+            }
+
+            _context.MileageRecords.Remove(foundMileageRecord);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
