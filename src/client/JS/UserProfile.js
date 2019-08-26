4bea185475266ef28f26d289d750d11f325bc1dd
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-26 13:49:48 -0500

    Completed AJAX to create user profile

---
+++ src/client/JS/UserProfile.js
@@ -0,0 +1,33 @@
+var uri = "https://localhost:5001/api/UserProfile";
+
+function CreateUserProfile(e) {
+  $(`[name="addFirstName"]`).empty();
+  $(`[name="addLastName"]`).empty();
+
+  const userProfile = {
+    FirstName: $(`#add-first-name`).val(),
+    LastName: $(`#add-last-name`).val()
+  };
+
+  if (userProfile.FirstName === "") {
+    $(`[name="addFirstName"]`).append("* First name is required.");
+    return;
+  } else if (userProfile.LastName === "") {
+    $(`[name="addLastName"]`).append("* Last name is required.");
+    return;
+  } else {
+    $.ajax({
+      type: "POST",
+      accepts: "application/json",
+      url: uri,
+      contentType: "application/json",
+      data: JSON.stringify(userProfile),
+      error: error => {
+        console.log(error);
+      },
+      success: function(result) {
+        console.log("Success");
+      }
+    });
+  }
+}
