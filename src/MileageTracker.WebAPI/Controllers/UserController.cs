3f39cab2edaa219fd66c05b06c0079c373a827a8
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-09-03 11:36:28 -0500

    Login and Register now creates a TOKEN to interact with the client side

--- src/MileageTracker.WebAPI/Controllers/UserController.cs
+++ src/MileageTracker.WebAPI/Controllers/UserController.cs
@@ -1,29 +1,46 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MileageTracker.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : Controller
+    public class UserController : MTBaseController
     {
         private readonly UserManager<IdentityUser> _userManager;
-        public UserController(UserManager<IdentityUser> userManager)
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Post(UserModel user)
+        public async Task<IActionResult> Register(UserModel user)
         {
+            var identityEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (user.Email == identityEmail)
+            {
+                return Ok(new
+                {
+                    success = false,
+                    message = "Email already exists!"
+                });
+            }
             var identityUser = new IdentityUser()
             {
                 UserName = user.Email,
                 Email = user.Email,
             };
+
             var result = await _userManager.CreateAsync(identityUser, user.Password);
 
             if (!result.Succeeded)
@@ -35,11 +52,70 @@ namespace MileageTracker.WebAPI.Controllers
                 });
             }
 
+            var userData = await _userManager.FindByNameAsync(user.Email);
+
+            var token = GenerateToken(userData);
+
             return Ok(new
             {
-                success = true,
-                message = "User created sucesfully."
+                Id = userData.Id,
+                Username = userData.UserName,
+                Email = userData.Email,
+                Token = token,
+                Success = true
             });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]UserModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
+
+            if (!result.Succeeded)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var user = await _userManager.FindByNameAsync(model.Email);
+
+            var token = GenerateToken(user);
+
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Email = user.Email,
+                Token = token,
+                Success = true
+            });
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(new
+            {
+                Success = true
+            });
+        }
+
+        public string GenerateToken(IdentityUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Startup.AuthenticationSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return tokenString;
         }
     }
 
