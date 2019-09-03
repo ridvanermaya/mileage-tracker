eaa18fea6fe86ee43aa41a59c1f8991cce7d231a
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-09-03 11:31:38 -0500

    Added configuration to use JWT

--- src/MileageTracker.WebAPI/Startup.cs
+++ src/MileageTracker.WebAPI/Startup.cs
@@ -8,11 +8,18 @@ using MileageTracker.WebAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Hangfire;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace MileageTracker.WebAPI
 {
     public class Startup
     {
+
+        public static string AuthenticationSecret = "RIDVAN-3455-TR-USA";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,14 +34,52 @@ namespace MileageTracker.WebAPI
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")
                 ));
-            
+
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            var key = Encoding.ASCII.GetBytes(AuthenticationSecret);
+            services.AddAuthentication(x =>
+                       {
+                           x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                           x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                       })
+                       .AddJwtBearer(x =>
+                       {
+                           x.Events = new JwtBearerEvents
+                           {
+                               OnTokenValidated = context =>
+                               {
+                                   var userService = context.HttpContext.RequestServices.GetRequiredService<UserManager<IdentityUser>>();
+                                   var userId = context.Principal.Identity.Name;
+                                   var user = userService.FindByIdAsync(userId).GetAwaiter().GetResult();
+                                   if (user == null)
+                                   {
+                                       // return unauthorized if user no longer exists
+                                       context.Fail("Unauthorized");
+                                   }
+                                   return Task.CompletedTask;
+                               }
+                           };
+                           x.RequireHttpsMetadata = false;
+                           x.SaveToken = true;
+                           x.TokenValidationParameters = new TokenValidationParameters
+                           {
+                               ValidateIssuerSigningKey = true,
+                               IssuerSigningKey = new SymmetricSecurityKey(key),
+                               ValidateIssuer = false,
+                               ValidateAudience = false
+                           };
+                       });
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddCors(options => {
-                options.AddDefaultPolicy(builder => {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
                     builder.WithOrigins("http://127.0.0.1:5500").AllowCredentials().AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
                 });
             });
